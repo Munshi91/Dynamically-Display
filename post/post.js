188a24fb74c09bd7a25ca025b7d2a2d06e66eb88
Munshi91 <mimunshi91@gmail.com>
2024-06-02 12:57:30 +0600

    Load Posts

---
+++ post/post.js
@@ -0,0 +1,20 @@
+function loadPosts() {
+  fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data));
+}
+function displayPosts(posts) {
+  const postContainer = document.getElementById('post-container');
+  for (const post of posts) {
+    const div = document.createElement('div');
+    div.classList.add('post');
+    div.innerHTML = `
+
+<h4>User- ${post.userId}</h4>
+<h4>Post: ${post.title}</h4>
+<p>Post Description : ${post.body}</p>
+    `;
+    postContainer.appendChild(div);
+  }
+}
+loadPosts();
